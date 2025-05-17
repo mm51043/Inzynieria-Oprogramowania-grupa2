cc7d33bbee2e03a7c076d0800df161b50d1b5a07
unknown <mm51043@zut.edu.pl>
2025-05-17 22:26:50 +0200

    pliki z bazy xampp

--- medimaxzut/cmake-build-release/.qt/QtDeployTargets.cmake
+++ medimaxzut/cmake-build-release/.qt/QtDeployTargets.cmake
@@ -1,3 +1,3 @@
-set(__QT_DEPLOY_TARGET_medimaxzut_FILE C:/Users/Lenovo/CLionProjects/medimaxzut/cmake-build-release/medimaxzut.exe)
+set(__QT_DEPLOY_TARGET_medimaxzut_FILE D:/Inzynieria-Oprogramowania-grupa2/medimaxzut/cmake-build-release/medimaxzut.exe)
 set(__QT_DEPLOY_TARGET_medimaxzut_TYPE EXECUTABLE)
 set(__QT_DEPLOY_TARGET_medimaxzut_RUNTIME_DLLS C:/Qt/6.9.0/msvc2022_64/bin/Qt6Core.dll)
