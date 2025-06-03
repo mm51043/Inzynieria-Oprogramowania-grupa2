8c1d3a7dba07a9065022579e23e4906ed5ad25ec
Milosz <km55843@zut.edu.pl>
2025-06-03 23:54:40 +0200

    env

---
+++ medimaxzut/cmake-build-debug-visual-studio/CMakeFiles/medimaxzut.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/login/login.cpp" "CMakeFiles/medimaxzut.dir/login/login.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/login/login.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/mailList/listmail.cpp" "CMakeFiles/medimaxzut.dir/mailList/listmail.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/mailList/listmail.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/mailList/mailext.cpp" "CMakeFiles/medimaxzut.dir/mailList/mailext.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/mailList/mailext.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/mailList/mailitem.cpp" "CMakeFiles/medimaxzut.dir/mailList/mailitem.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/mailList/mailitem.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/main.cpp" "CMakeFiles/medimaxzut.dir/main.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/main.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/mainWindow/mainwindow.cpp" "CMakeFiles/medimaxzut.dir/mainWindow/mainwindow.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/mainWindow/mainwindow.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/mainWindow/navibutton.cpp" "CMakeFiles/medimaxzut.dir/mainWindow/navibutton.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/mainWindow/navibutton.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/cmake-build-debug-visual-studio/medimaxzut_autogen/mocs_compilation.cpp" "CMakeFiles/medimaxzut.dir/medimaxzut_autogen/mocs_compilation.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/medimaxzut_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/prescriptionAdd/prescriptionitem.cpp" "CMakeFiles/medimaxzut.dir/prescriptionAdd/prescriptionitem.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/prescriptionAdd/prescriptionitem.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/prescriptionAdd/prescriptionwindow.cpp" "CMakeFiles/medimaxzut.dir/prescriptionAdd/prescriptionwindow.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/prescriptionAdd/prescriptionwindow.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/userList/listitem.cpp" "CMakeFiles/medimaxzut.dir/userList/listitem.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/userList/listitem.cpp.obj.d"
+  "C:/Users/milos/Inzynieria-Oprogramowania-grupa2/medimaxzut/userList/listuser.cpp" "CMakeFiles/medimaxzut.dir/userList/listuser.cpp.obj" "msvc" "CMakeFiles/medimaxzut.dir/userList/listuser.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ medimaxzut/cmake-build-debug-visual-studio/CMakeFiles/medimaxzut.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/medimaxzut_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/medimaxzut_autogen.dir/ParseCache.txt"
+  "medimaxzut_autogen"
+  "CMakeFiles/medimaxzut.dir/login/login.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/login/login.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/mailList/listmail.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/mailList/listmail.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/mailList/mailext.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/mailList/mailext.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/mailList/mailitem.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/mailList/mailitem.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/main.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/main.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/mainWindow/mainwindow.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/mainWindow/mainwindow.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/mainWindow/navibutton.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/mainWindow/navibutton.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/medimaxzut_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/medimaxzut_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/prescriptionAdd/prescriptionitem.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/prescriptionAdd/prescriptionitem.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/prescriptionAdd/prescriptionwindow.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/prescriptionAdd/prescriptionwindow.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/userList/listitem.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/userList/listitem.cpp.obj.d"
+  "CMakeFiles/medimaxzut.dir/userList/listuser.cpp.obj"
+  "CMakeFiles/medimaxzut.dir/userList/listuser.cpp.obj.d"
+  "medimaxzut.exe"
+  "medimaxzut.exe.manifest"
+  "medimaxzut.lib"
+  "medimaxzut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/medimaxzut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
